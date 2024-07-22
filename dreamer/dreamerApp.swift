3bc5eac50c5a2d66c0212d4aad8f4031b7527e4f
kodychik <73432901+kodychik@users.noreply.github.com>
2024-07-22 14:02:39 +0800

    Initial Commit

---
+++ dreamer/dreamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  dreamerApp.swift
+//  dreamer
+//
+//  Created by Kody Chik on 2024-07-22.
+//
+
+import SwiftUI
+
+@main
+struct dreamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
